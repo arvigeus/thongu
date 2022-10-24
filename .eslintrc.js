d09d74230e278ab18811c84d553b82def6093fd4
Nikolay Stoynov <arvigeus@gmail.com>
2022-10-24 11:55:42 +0700

    feat(eslint): add support for tailwindcss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
+  plugins: ["tailwindcss"],
   extends: [
     "@remix-run/eslint-config",
     "prettier",
+    "plugin:tailwindcss/recommended",
   ],
   ignorePatterns: [
     ".cache",
     "/node_modules",
   ],
+  settings: {
+    tailwindcss: {
+      config: "twind.config.ts",
+    },
+  },
 };
--- layout.tsx
+++ layout.tsx
@@ -3,7 +3,7 @@ import type { ComponentChildren } from "preact";
 export default function Layout({ children }: { children: ComponentChildren }) {
   return (
     <div
-      class="min-w-full min-h-screen bg-vn-red text-vn-yellow"
+      class="bg-vn-red text-vn-yellow min-w-full min-h-screen"
       style={{ textShadow: "0 0 1px darkblue" }}
     >
       <div class="p-4 mx-auto max-w-screen-md flex flex-col">
