92cf85d11b35b89d715e95efbd8ca58119771fc8
Nikolay Stoynov <arvigeus@gmail.com>
2022-10-24 14:37:32 +0300

    style: apply red color

--- layout.tsx
+++ layout.tsx
@@ -3,8 +3,7 @@ import type { ComponentChildren } from "preact";
 export default function Layout({ children }: { children: ComponentChildren }) {
   return (
     <div
-      class="bg-gradient-to-r from-red-700 to-red-900
-      text-vn-yellow min-w-full min-h-screen"
+      class="bg-vn-red text-vn-yellow min-w-full min-h-screen"
       style={{ textShadow: "0 0 1px darkblue" }}
     >
       <div class="p-4 mx-auto max-w-screen-md flex flex-col">
