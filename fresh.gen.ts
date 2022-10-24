0adee26e0e75b03172142b2b751426d4b34b404b
Nikolay Stoynov <arvigeus@gmail.com>
2022-10-24 12:39:21 +0700

    style: change favicon

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,9 +6,10 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
-import * as $$0 from "./islands/LetterInput.tsx";
-import * as $$1 from "./islands/MultiCharacterInput.tsx";
-import * as $$2 from "./islands/WordInput.tsx";
+import * as $$0 from "./islands/InputPrompt.tsx";
+import * as $$1 from "./islands/LetterInput.tsx";
+import * as $$2 from "./islands/MultiCharacterInput.tsx";
+import * as $$3 from "./islands/WordInput.tsx";
 
 const manifest = {
   routes: {
@@ -17,9 +18,10 @@ const manifest = {
     "./routes/index.tsx": $2,
   },
   islands: {
-    "./islands/LetterInput.tsx": $$0,
-    "./islands/MultiCharacterInput.tsx": $$1,
-    "./islands/WordInput.tsx": $$2,
+    "./islands/InputPrompt.tsx": $$0,
+    "./islands/LetterInput.tsx": $$1,
+    "./islands/MultiCharacterInput.tsx": $$2,
+    "./islands/WordInput.tsx": $$3,
   },
   baseUrl: import.meta.url,
   config,
